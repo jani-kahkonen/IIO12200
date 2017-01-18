0934e872f84d6e45b02f12c21d16084e4a77e812
H9575 <H9575@MEDI-406-WS-04>
2017-01-18 17:22:46 +0200

    Initial commit.

---
+++ Protot/Proto_Haku/Proto_Haku.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Proto_Haku.aspx.cs" Inherits="Proto_Haku" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title>Proto_Haku</title>
+    </head>
+    
+    <body>
+        <form id="form" runat="server">
+            <div>
+                <asp:TextBox ID="txtSearch" runat="server" />
+            </div>
+        </form>
+    </body>
+</html>
---
+++ Protot/Proto_Haku/Proto_Haku.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Proto_Haku : System.Web.UI.Page
+{
+    public struct Document
+    {
+        public string name;
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Load documents.
+        List<Document> documents = new List<Document>()
+        {
+            new Document { name = "Dokumentti_01" },
+            new Document { name = "Dokumentti_02" },
+            new Document { name = "Dokumentti_03" }
+        };
+    }
+
+    public static void Filter()
+    {
+        // Filter documents.
+    }
+}
+
+// http://www.infinetsoft.com/Post/How-to-create-autocomplete-textbox-with-database-in-asp-net-c/1254#.WH901Vw6xlw
+
+// http://www.aspsnippets.com/Articles/Implement-jQuery-AutoComplete-TextBox-from-database-using-AJAX-PageMethods-in-ASPNet.aspx
+
+// http://www.dotnetawesome.com/2013/12/autocomplete-textbox-without-webservice.html
